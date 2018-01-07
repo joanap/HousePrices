66ca6bb00e9e6493833773305f3bae4891bdadd1
AlexGomesDS <Alexsantogomes@gmail.com>
2018-01-07 20:42:49 +0000

    [UPD] - Add LogSalePrice to the predicted columns. Created new assertions

--- src/features/preprocessor/mean_mode_preprocessor.py
+++ src/features/preprocessor/mean_mode_preprocessor.py
@@ -35,7 +35,8 @@ class Mean_Mode_Preprocessor(AbstractPreprocessor):
     # For categorical features, replace nan with the string "NaN" and apply labelencoding 
     #(also add the value Nan in case it appears in the test set and not in the train)
     def _gen_cat_col_encoder(self, col_name, df=pd.DataFrame()):
-        return LabelEncoder().fit(df[col_name].fillna('NaN').append(pd.DataFrame(['NaN'])).values.ravel())
+        col_with_NaN = df[col_name].fillna('NaN').append(pd.DataFrame(['NaN']))
+        return LabelEncoder().fit(col_with_NaN.values.ravel())
     
     # Check if this is training set by looking for the output column called "SalePrice"
     def _is_trainning_set(self, dataframe):
@@ -51,20 +52,27 @@ class Mean_Mode_Preprocessor(AbstractPreprocessor):
     
     # Create a column with the log of the saleprice
     def _feat_eng_train(self, dataframe):
-        for col in self.get_cols_to_predict():
-            dataframe["Log_" + col] = np.log(dataframe[col])
+        cols_to_apply_log = list(self.get_cols_to_predict())
+        for col_name in cols_to_apply_log:
+            dataframe["Log_" + col_name] = np.log(dataframe[col_name])
+            self.append_cols_to_predict("Log_" + col_name)
 
 
 #%% Testing
 
 if __name__ == '__main__':
+    data_preprocessor = Mean_Mode_Preprocessor(["SalePrice"])
+    
+
     train_dataframe = pd.read_csv('..\\input\\train.csv', index_col='Id')
     test_dataframe = pd.read_csv("..\\input\\test.csv", index_col='Id')
 
-    data_preprocessor = Mean_Mode_Preprocessor(["SalePrice"])
-    X_train, y_train = data_preprocessor.prepare_and_cook(train_dataframe)
-    X_test = data_preprocessor.cook(test_dataframe)
+    data_preprocessor.prepare(train_dataframe)
+    X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
+    X_test, _ = data_preprocessor.cook_and_split(test_dataframe)
     #tests
     #test if there are any NaNs
     assert(X_train.isnull().sum().sum() == 0)
     assert(X_test.isnull().sum().sum() == 0)
+    assert(X_train.select_dtypes(exclude = [np.number]).shape[1] == 0)
+    assert(X_test.select_dtypes(exclude = [np.number]).shape[1] == 0)
