05ce9aef8d65d06d44904262244773c4724f17a5
ASSG <ASSG@xpand-it.com>
2018-01-17 14:53:17 +0000

    [UPD] - Simplified the code

--- src/features/preprocessor/mean_mode_preprocessor.py
+++ src/features/preprocessor/mean_mode_preprocessor.py
@@ -21,38 +21,29 @@ from sklearn.preprocessing import LabelEncoder
 #%% Implementing class with our one version of the preprocessing methods
 
 class Mean_Mode_Preprocessor(AbstractPreprocessor):
-    
-    # replace missing numerical values with the mean
-    def _calc_missing_num_replacements(self, col):
-        return col.mean()
 
     # replace missing labels with the most common one (only when the NaNs are less than 10% of the full dataset)
-    def _calc_missing_cat_replacements(self, col):
+    def _set_missing_cat_replacements(self, col):
         if col.count() / col.shape[0] > 0.90:
             return col.value_counts().index[0]
         return 'NaN'
     
+    # set of dependent variables is only the last one (the one added in feat. eng.)
+    def get_y(self, df):
+        return df.loc[:, self.get_cols_to_predict()[-1]]
+    
     # For categorical features, replace nan with the string "NaN" and apply labelencoding 
-    #(also add the value Nan in case it appears in the test set and not in the train)
-    def _gen_cat_col_encoder(self, col_name, df=pd.DataFrame()):
-        col_with_NaN = df[col_name].fillna('NaN').append(pd.DataFrame(['NaN']))
-        return LabelEncoder().fit(col_with_NaN.values.ravel())
-    
-    # Check if this is training set by looking for the output column called "SalePrice"
-    def _is_trainning_set(self, dataframe):
-        for col in self.get_cols_to_predict():
-            if col not in dataframe.columns:
-                return False
-        return True
-        #return self.get_col_to_predict() in dataframe.columns
-    
-    # Don't create any new features for both datasets
-    def _feat_eng(self, dataframe):
-        pass
+    # (also add the value Nan in case it appears in the test set and not in the train)
+    def _set_cat_col_encoder(self, df):
+        self.cat_encoders = {}
+        for col_name in self.categorical_features:
+            col_with_NaN = df[col_name].fillna('NaN').append(pd.DataFrame(['NaN']))
+            self.cat_encoders[col_name] = LabelEncoder().fit(col_with_NaN.values.ravel())
     
     # Create a column with the log of the saleprice
     def _feat_eng_train(self, dataframe):
         cols_to_apply_log = list(self.get_cols_to_predict())
+        
         for col_name in cols_to_apply_log:
             dataframe["Log_" + col_name] = np.log(dataframe[col_name])
             self.append_cols_to_predict("Log_" + col_name)
@@ -62,17 +53,26 @@ class Mean_Mode_Preprocessor(AbstractPreprocessor):
 
 if __name__ == '__main__':
     data_preprocessor = Mean_Mode_Preprocessor(["SalePrice"])
-    
 
     train_dataframe = pd.read_csv('..\\input\\train.csv', index_col='Id')
     test_dataframe = pd.read_csv("..\\input\\test.csv", index_col='Id')
 
     data_preprocessor.prepare(train_dataframe)
     X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
+    data_preprocessor.prepare(train_dataframe)
+    X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
     X_test, _ = data_preprocessor.cook_and_split(test_dataframe)
+    
     #tests
     #test if there are any NaNs
     assert(X_train.isnull().sum().sum() == 0)
     assert(X_test.isnull().sum().sum() == 0)
+    
+    #test the outputs have the correct shapes
+    assert(X_train.shape == (1460, 79))
+    assert(y_train.shape == (1460,))
+    assert(X_test.shape == (1459, 79))
+    
+    # test every column in the outputs is of numeric type
     assert(X_train.select_dtypes(exclude = [np.number]).shape[1] == 0)
     assert(X_test.select_dtypes(exclude = [np.number]).shape[1] == 0)
--- src/features/preprocessor/mean_price_label_encoder.py
+++ src/features/preprocessor/mean_price_label_encoder.py
@@ -17,7 +17,7 @@ from features.preprocessor.abstract_preprocessor import AbstractPreprocessor
 import numpy as np
 import pandas as pd
 
-#%%
+#%% Class to assign the mean value of the predicted column to each label of a cat. feature
 
 class mean_pred_encoder():
     def __init__(self, col_y_name, cat_col_name):
@@ -31,6 +31,7 @@ class mean_pred_encoder():
         new_df.index = new_df.vals
         new_df['mean'] = df[[self.col_name, self.feature_to_predict]].groupby(self.col_name).mean()[self.feature_to_predict]
         self.means = new_df.fillna(new_df.mean())['mean'].to_dict()
+        return self
             
     def transform(self, col):
         new_col = col.copy()
@@ -41,41 +42,30 @@ class mean_pred_encoder():
 #%% Implementing class with our one version of the preprocessing methods
 
 class Mean_Price_Preprocessor(AbstractPreprocessor):
+    # generate attribute that will store all the encoders in a dictionary
+    def _set_cat_col_encoder(self, df):
+        self.cat_encoders = {}
+        for col_name in self.categorical_features:
+            encoder = mean_pred_encoder(self.get_init_cols_to_predict()[0], col_name)
+            self.cat_encoders[col_name] = encoder.fit(df.fillna('NaN'))
     
-    # replace missing numerical values with the mean
-    def _calc_missing_num_replacements(self, col):
-        return col.mean()
-
-    # replace missing labels with the most common one (only when the NaNs are less than 10% of the full dataset)
-    def _calc_missing_cat_replacements(self, col):
-        return 'NaN'
-    
-    def _gen_cat_col_encoder(self, col_name, df = pd.DataFrame()):
-        col_encoder = mean_pred_encoder(self.get_init_cols_to_predict()[0], col_name)
-        col_encoder.fit(df.fillna('NaN'))
-        return col_encoder
-    
-    # Check if this is training set by looking for the output column called "SalePrice"
-    def _is_trainning_set(self, dataframe):
-        for col in self.get_init_cols_to_predict() + self.get_cols_to_predict():
-            if col in dataframe.columns:
-                return True
-        return False
-
-    # Create a column with the log of the saleprice
-    def _feat_eng_train(self, dataframe):
-        #use list() to create a new instance
-        cols_to_apply_log = list(self.get_init_cols_to_predict())
-        
-        for col_name in cols_to_apply_log:
-            dataframe["Log_" + col_name] = np.log(dataframe[col_name])
-            dataframe.drop(col_name, 1, inplace = True)
-            self.set_cols_to_predict(["Log_" + col_name])
-            
+    # convert every feature to numeric (necessary to train regression models later)
     def _feat_eng(self, dataframe):
         for col_name in dataframe.select_dtypes(exclude = [np.number]):
             dataframe[col_name] = pd.to_numeric(dataframe[col_name])
             
+    # Create a column with the log of the predict column
+    def _feat_eng_train(self, dataframe):        
+        for col_name in self.get_init_cols_to_predict():
+            dataframe["Log_" + col_name] = np.log(dataframe[col_name])
+            
+            #don't forget to add it to the list of columns not present in the test set!
+            self.append_cols_to_predict("Log_" + col_name)
+            
+    # set of dependent variables is only the last one (the one added in feat. eng.)
+    def get_y(self, df):
+       return df.loc[:, self.get_cols_to_predict()[-1]]
+    
 
 #%% Testing
 
@@ -87,10 +77,20 @@ if __name__ == '__main__':
 
     data_preprocessor.prepare(train_dataframe)
     X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
+    data_preprocessor.prepare(train_dataframe)
+    X_train, y_train = data_preprocessor.cook_and_split(train_dataframe)
     X_test, _ = data_preprocessor.cook_and_split(test_dataframe)
+    
     #tests
     #test if there are any NaNs
     assert(X_train.isnull().sum().sum() == 0)
     assert(X_test.isnull().sum().sum() == 0)
+    
+    #test the outputs have the correct shapes
+    assert(X_train.shape == (1460, 79))
+    assert(y_train.shape == (1460,))
+    assert(X_test.shape == (1459, 79))
+    
+    # test every column in the outputs is of numeric type
     assert(X_train.select_dtypes(exclude = [np.number]).shape[1] == 0)
     assert(X_test.select_dtypes(exclude = [np.number]).shape[1] == 0)
